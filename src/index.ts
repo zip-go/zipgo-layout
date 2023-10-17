115c6b5f4e9e5d580ff9bb6bd75a3f01a1290450
SangwonKang <72087183+ksone02@users.noreply.github.com>
2023-10-17 18:16:54 +0900

    refactor: DesktopView export 방식 변경

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import DesktopView from './components/DesktopView/DesktopView';
 import useDesktopView from './hooks/useDesktopView';
 
-export { DesktopView as default, useDesktopView };
+export { DesktopView, useDesktopView };
